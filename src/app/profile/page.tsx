3c6bbbf3ba23315f14f46baadd064d54e74ccca2
Chris Galgano <chrisgalgano@MacBook-Pro-223.local>
2025-09-17 14:33:53 -0400

    added app skeleton

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>This is where we will have info about the user's account and also provide their previous recordings
+      and other relevant information. Placeholder for now.
+      </p>
+    </div>
+  )
+}
